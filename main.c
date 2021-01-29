fec3718174d5494d0520f9a877d7cc8b9adf253c
LongJack00 <235898@edu.p.lodz.pl>
2021-01-29 01:21:43 +0100

    zmiana mnożenia w metodzie policz

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int policz(int, int);
+
 int main() {
     printf("Hello, World!\n");
+    printf("%d\n", policz(5, 6));
     return 0;
+}
+
+int policz(int a, int b)
+{
+    return a * b;
 }
