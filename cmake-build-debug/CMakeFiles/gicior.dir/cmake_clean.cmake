5c4e960840f1c486bd8363fce97c6daae9829622
LongJack00 <235898@edu.p.lodz.pl>
2021-01-29 01:12:18 +0100

    commmit

---
+++ cmake-build-debug/CMakeFiles/gicior.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gicior.dir/main.c.obj"
+  "gicior.exe"
+  "gicior.exe.manifest"
+  "gicior.pdb"
+  "libgicior.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gicior.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
